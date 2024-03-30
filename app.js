ac2f99275e9e7de23a2ae4bc1c872c819482ce36
Sergio <137483700+KaratSergio@users.noreply.github.com>
2024-03-30 20:05:15 +0200

    start set

---
+++ app.js
@@ -0,0 +1,34 @@
+import express from "express";
+import morgan from "morgan";
+import cors from "cors";
+
+import usersRouter from "./routes/usersRouter.js";
+
+import { connectDb } from "./db/connection.js";
+
+import contactsRouter from "./routes/contactsRouter.js";
+
+const { PORT } = process.env;
+
+const app = express();
+
+app.use(morgan("tiny"));
+app.use(cors());
+app.use(express.json());
+app.use("users", usersRouter);
+app.use("/api/contacts", contactsRouter);
+
+app.use((_, res) => {
+  res.status(404).json({ message: "Route not found" });
+});
+
+app.use((err, req, res, next) => {
+  const { status = 500, message = "Server error" } = err;
+  res.status(status).json({ message });
+});
+
+connectDb().then(() => {
+  app.listen(PORT, () => {
+    console.log(`"Server is running. Use our API on port: ${PORT}"`);
+  });
+});
