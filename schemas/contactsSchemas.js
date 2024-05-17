c43aea725393c51919fd56550b3ebc9e4468fec0
Sergio <137483700+KaratSergio@users.noreply.github.com>
2024-05-17 13:18:40 +0300

    start set

--- schemas/contactsSchemas.js
+++
@@ -1,6 +0,0 @@
-import Joi from "joi";
-
-export const createContactSchema = Joi.object({
-  name: Joi.string().required(),
-  number: Joi.string().required(),
-});
--- services/contactsServices.js
+++
@@ -1,6 +0,0 @@
-import { Contact } from "../db/models/Contact.js";
-
-export const getAllContactsService = (owner) => Contact.find({ owner });
-export const createContactService = (data) => Contact.create(data);
-export const deleteContactsService = (filter) =>
-  Contact.findOneAndDelete(filter);
